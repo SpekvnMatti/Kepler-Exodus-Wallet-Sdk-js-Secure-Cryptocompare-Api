55351a6b18a04fa360ab0d7d588cacfb74de5d2a
housedesi0-ux <housedesi0@gmail.com>
2026-04-27 22:19:34 +0300

    broken-required.js

---
+++ Kepler-Exodus-Wallet/regressions/broken-required.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const tape = require('tape')
+const { validator } = require('../../')
+
+const runWithOptions = (t, opts) => {
+  t.notOk(validator({ required: [], uniqueItems: true }, opts)([1, 1]), 'required + uniqueItems')
+  t.end()
+}
+
+tape('default', (t) => runWithOptions(t, {}))
+tape('allErrors', (t) => runWithOptions(t, { includeErrors: true, allErrors: true }))
